feeca29b3ebd17b6cf06c581cfe110f6ebc2c5c1
aghilazghi <amineteclai@gmail.com>
2020-11-27 18:23:08 -0500

    Add controls event binding and listerners

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'password-generator';
+  
+  password = "";
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  length = 0;
+
+  onLengthChange(value: string){
+    const parsedValue = parseInt(value);
+    this.length = parsedValue === NaN ? 0 : parsedValue;
+  }
+
+  onUseLettersChange(){
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onUseNumbersChange(){
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+   onUseSymbolsChange(){
+    this.includeSymbols = !this.includeSymbols;
+  }
+
+  onButtonClick(){    
+  } 
+ 
 }
