1f282b626ef8b0436b61ce081366c4511d28b3a9
aghilazghi <amineteclai@gmail.com>
2020-11-27 18:56:52 -0500

    Add logic of generating random password

--- password-generator/src/app/app.component.ts
+++ password-generator/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { IfStmt } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -15,7 +16,9 @@ export class AppComponent {
 
   onLengthChange(value: string){
     const parsedValue = parseInt(value);
-    this.length = parsedValue === NaN ? 0 : parsedValue;
+    if(!isNaN(parsedValue)){
+      this.length = parsedValue;
+    }
   }
 
   onUseLettersChange(){
@@ -30,7 +33,33 @@ export class AppComponent {
     this.includeSymbols = !this.includeSymbols;
   }
 
-  onButtonClick(){    
+  onButtonClick(){
+    const numbers = "1234567890";
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    const symbols = "!@#$^&*";    
+    let validChars = "";
+    let randomGeneratedPassword = "";
+
+    if(this.includeLetters){
+      validChars += letters;
+    }
+
+    if(this.includeNumbers){
+      validChars += numbers;
+    }
+
+    if(this.includeSymbols){
+      validChars += symbols;
+    }
+
+    for(let i = 0; i < this.length; i++){
+      const randomIndex = Math.floor(Math.random() * validChars.length);
+
+      randomGeneratedPassword += validChars[randomIndex];
+    }
+
+    this.password = randomGeneratedPassword;
+    
   } 
  
 }
